ebd142350ecf341491a32f917000e0a4f4d29b85
elenakout <elenakout@gmail.com>
2020-02-12 21:48:52 +0200

    :pencil: [dev] Connect to DB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Routes
-const bootcamps = require('./api/v1/bootcambs');
+const connectDB = require('../config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Routes
+const bootcamps = require('./api/v1/bootcambs');
 
 const app = express();
 
@@ -24,7 +28,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`App listening in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
