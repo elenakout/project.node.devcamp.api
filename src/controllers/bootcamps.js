e5e1f28a9d1a67d2395596cabcec4c8e048fe48c
elenakout <elenakout@gmail.com>
2020-04-10 19:32:25 +0300

    sorting

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = async (req, res, next) => {
     const reqQuery = { ...req.query };
 
     // Fields to exclude
-    const removeFields = ['select'];
+    const removeFields = ['select', 'sort'];
 
     // Loop over removeFields and delete them from reqQuery
     removeFields.forEach((param) => delete reqQuery[param]);
@@ -35,6 +35,15 @@ exports.getBootcamps = async (req, res, next) => {
       const fields = req.query.select.split(',').join(' ');
 
       query = query.select(fields);
+    }
+
+    // Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
     }
 
     // Executing query
