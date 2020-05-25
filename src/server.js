d86713f8a3dbea7f183f2e1bd48f25f8af4161cf
elenakout <elenakout@gmail.com>
2020-05-25 21:01:40 +0300

    [video]12. Photo Upload For Bootcamp

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 
 const connectDB = require('../config/db');
@@ -29,6 +30,9 @@ app.get('/', (req, res) => {
     message: 'hello world',
   });
 });
+
+// File uploading
+app.use(fileupload());
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
