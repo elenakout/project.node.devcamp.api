910aaef272ea0e9ede03755a7ef025105573d803
elenakout <elenakout@gmail.com>
2020-07-19 19:47:07 +0300

    [video#7]6. Auth Protect Middleware

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -13,6 +13,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router
   .route('/')
   .get(
@@ -22,8 +24,12 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, addCourse);
 
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
