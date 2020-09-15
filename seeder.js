bb68b98a009383da73a013c9d74b046172c3fc8d
elenakout <elenakout@gmail.com>
2020-09-15 19:07:00 +0300

    [video#9] 2. Get Single Review  Update Seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./src/models/Bootcamp');
 const Course = require('./src/models/Course');
 const User = require('./src/models/User');
+const Review = require('./src/models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -37,6 +38,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/src/_data/users.json`, 'utf-8')
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/src/_data/reviews.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
@@ -44,6 +48,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
 
     console.log('Data Imported...');
     process.exit();
@@ -58,6 +63,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
 
     console.log('Data Destroyed...');
     process.exit();
--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,13 +1,16 @@
-const { Router } = require('express');
+const express = require('express');
 const {
   getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
-const router = Router({ mergeParams: true });
+const router = express.Router({ mergeParams: true });
 
-// Advanced Results
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
@@ -16,12 +19,16 @@ router
   .get(
     advancedResults(Review, {
       path: 'bootcamp',
-      select: 'name description',
+      select: 'name description'
     }),
     getReviews
   )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router
   .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
